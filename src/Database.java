e17e7bb5112055b79502988ff44b8f69062f30b9
shadenrafik <es.shadn.rafik2023@alexu.edu.eg>
2025-10-18 19:42:06 +0300

    Create Database.java

---
+++ src/Database.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+
+public abstract class Database<T> {
+    protected ArrayList<T> records = new ArrayList<>();
+    protected String filename;
+
+    public Database(String filename) {
+        this.filename = filename;
+    }
+
+    public abstract void readFromFile();
+    public abstract T createRecordFrom(String line);
+    public abstract void saveToFile();
+
+    public ArrayList<T> returnAllRecords() {
+        return records;
+    }
+
+    public void insertRecord(T record) {
+        records.add(record);
+    }
+
+    public void deleteRecord(T record) {
+        records.remove(record);
+    }
+
+    public boolean contains(T record) {
+        return records.contains(record);
+    }
+}

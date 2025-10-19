88964b2d480bc9272f6096b276e60d451be0494c
sarahanyy6 <sararakha2322006@gmail.com>
2025-10-19 21:51:01 +0300

    fixed while loops

--- lab4/src/Main.java
+++ lab4/src/Main.java
@@ -89,8 +89,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-
-
         while (true) {
             System.out.println("\n=== Inventory Management System ===");
             System.out.println("1. Admin");
@@ -102,111 +100,132 @@ public class Main {
             switch(choice) {
                 case 1 ->{
                     AdminRole admin = new AdminRole();
-                    System.out.println("\n=== Inventory Management System ===");
-                    System.out.println("1. Admin - Add Employee");
-                    System.out.println("2. Admin - Remove Employee");
-                    System.out.println("3. Admin - List Employees");
-                    System.out.println("4. Admin - Logout");
-                    System.out.println("0. Exit");
-                    System.out.print("Choose an option: ");
+                    while (true){
+                        System.out.println("\n=== Inventory Management System ===");
+                        System.out.println("1. Admin - Add Employee");
+                        System.out.println("2. Admin - Remove Employee");
+                        System.out.println("3. Admin - List Employees");
+                        System.out.println("4. Admin - Logout");
+                        System.out.println("0. Exit");
+                        System.out.print("Choose an option: ");
 
-                    int choice1 = validateInteger("");
+                        int choice1 = validateInteger("");
 
-                    switch (choice1) {
-                        case 1 -> {
-                            String id = validateNonEmpty("Employee ID: ");
-                            String name = validateName("Name: ");
-                            String email = validateEmail("Email: ");
-                            String address = validateNonEmpty("Address: ");
-                            String phone = validatePhone("Phone: ");
-                            admin.addEmployee(id, name, email, address, phone);
-                        }
-                        case 2 -> {
-                            String id = validateNonEmpty("Employee ID to remove: ");
-                            admin.removeEmployee(id);
-                        }
-                        case 3 -> {
-                            EmployeeUser[] employees = admin.getListOfEmployees();
-                            for (EmployeeUser e : employees) {
-                                System.out.println(e.lineRepresentation());
+                        switch (choice1) {
+                            case 1 -> {
+                                String id = validateNonEmpty("Employee ID: ");
+                                String name = validateName("Name: ");
+                                String email = validateEmail("Email: ");
+                                String address = validateNonEmpty("Address: ");
+                                String phone = validatePhone("Phone: ");
+                                admin.addEmployee(id, name, email, address, phone);
                             }
-                        }
-                        case 4 -> {
-                            admin.logout();
-                        }
+                            case 2 -> {
+                                String id = validateNonEmpty("Employee ID to remove: ");
+                                admin.removeEmployee(id);
+                            }
+                            case 3 -> {
+                                EmployeeUser[] employees = admin.getListOfEmployees();
+                                for (EmployeeUser e : employees) {
+                                    System.out.println(e.lineRepresentation());
+                                }
+                            }
+                            case 4 -> {
+                                admin.logout();
+                                System.out.println("Logged out. Returning to main menu");
 
-                        case 0 -> {
-                            admin.logout();
+                            }
 
-                            System.out.println("System exited. Data saved.");
-                            return;
+                            case 0 -> {
+                                admin.logout();
+
+                                System.out.println("System exited. Data saved.");
+                                return;
+                            }
+                            default -> System.out.println("Invalid option.");
                         }
-                        default -> System.out.println("Invalid option.");
-                    }
-                }
+                        if (choice1 == 4) break;
+                    }}
                 case 2 ->{
-                    EmployeeRole employee = new EmployeeRole(); System.out.println("1. Employee - Add Product");
-                    System.out.println("2. Employee - List Products");
-                    System.out.println("3. Employee - Purchase Product");
-                    System.out.println("4. Employee - Return Product");
-                    System.out.println("5. Employee - Apply Payment");
-                    System.out.println("6. Employee - List Purchases");
-                    System.out.println("0. Exit");
-                    System.out.print("Choose an option: ");
-                    int choice2 = validateInteger("");
+                    EmployeeRole employee = new EmployeeRole();
+                    while (true){
+                        System.out.println("1. Employee - Add Product");
+                        System.out.println("2. Employee - List Products");
+                        System.out.println("3. Employee - Purchase Product");
+                        System.out.println("4. Employee - Return Product");
+                        System.out.println("5. Employee - Apply Payment");
+                        System.out.println("6. Employee - List Purchases");
+                        System.out.println(("7. Employee - Logout"));
+                        System.out.println("0. Exit");
+                        System.out.print("Choose an option: ");
+                        int choice2 = validateInteger("");
 
-                    switch (choice2) {
-                        case 1 -> {
-                            String productID = validateNonEmpty("Product ID: ");
-                            String productName = validateName("Product Name: ");
-                            String manuName = validateName("Manufacturer: ");
-                            String suppName = validateName("Supplier: ");
-                            int quantity = validateInteger("Quantity: ");
-                            float price = validateFloat("Price: ");
-                            employee.addProduct(productID,productName,  manuName,  suppName,  quantity,  price);
-                        }
-                        case 2 -> {
-                            Product[] products = employee.getListOfProducts();
-                            for (Product p : products) {
-                                System.out.println(p.lineRepresentation());
+                        switch (choice2) {
+                            case 1 -> {
+                                String productID = validateNonEmpty("Product ID: ");
+                                String productName = validateName("Product Name: ");
+                                String manuName = validateName("Manufacturer: ");
+                                String suppName = validateName("Supplier: ");
+                                int quantity = validateInteger("Quantity: ");
+                                float price = validateFloat("Price: ");
+                                employee.addProduct(productID,productName,  manuName,  suppName,  quantity,  price);
                             }
+                            case 2 -> {
+                                Product[] products = employee.getListOfProducts();
+                                for (Product p : products) {
+                                    System.out.println(p.lineRepresentation());
+                                }
+                            }
+                            case 3 -> {
+                                String ssn = validateNonEmpty("Customer SSN: ");
+                                String prodID = validateNonEmpty("Product ID: ");
+                                LocalDate date = validateData("Purchase Date (dd-MM-yyyy): ");
+                                boolean success = employee.purchaseProduct(ssn, prodID, date);
+                                System.out.println(success ? "Purchase successful." : "Purchase failed.");
+                            }
+                            case 4 -> {
+                                String ssn = validateNonEmpty("Customer SSN: ");
+                                String prodID = validateNonEmpty("Product ID: ");
+                                LocalDate purchaseDate = validateData("Purchase Date (dd-MM-yyyy): ");
+                                LocalDate returnDate = validateData("Return Date (dd-MM-yyyy): ");
+                                double refund = employee.returnProduct(ssn, prodID, purchaseDate, returnDate);
+                                System.out.println(refund == -1 ? "Return failed." : "Return successful. Refund: " + refund);
+                            }
+                            case 5 -> {
+                                String ssn = validateNonEmpty("Customer SSN: ");
+                                LocalDate date = validateData("Purchase Date (dd-MM-yyyy): ");
+                                boolean paid = employee.applyPayment(ssn, date);
+                                System.out.println(paid ? "Payment applied." : "Payment failed.");
+                            }
+                            case 6 -> {
+                                CustomerProduct[] purchases = employee.getListOfPurchasingOperations();
+                                for (CustomerProduct cp : purchases) {
+                                    System.out.println(cp.lineRepresentation());}}
+                            case 7 -> {
+                                employee.logout();
+                                System.out.println("Logged out. Returning to main menu");
+                            }
+                            case 0 -> {
+                                employee.logout();
+                                System.out.println("System exited. Data saved.");
+                                return;
+                            }
+                            default -> System.out.println("Invalid option. Try again.");
                         }
-                        case 3 -> {
-                            String ssn = validateNonEmpty("Customer SSN: ");
-                            String prodID = validateNonEmpty("Product ID: ");
-                            LocalDate date = validateData("Purchase Date (dd-MM-yyyy): ");
-                            boolean success = employee.purchaseProduct(ssn, prodID, date);
-                            System.out.println(success ? "Purchase successful." : "Purchase failed.");
-                        }
-                        case 4 -> {
-                            String ssn = validateNonEmpty("Customer SSN: ");
-                            String prodID = validateNonEmpty("Product ID: ");
-                            LocalDate purchaseDate = validateData("Purchase Date (dd-MM-yyyy): ");
-                            LocalDate returnDate = validateData("Return Date (dd-MM-yyyy): ");
-                            double refund = employee.returnProduct(ssn, prodID, purchaseDate, returnDate);
-                            System.out.println(refund == -1 ? "Return failed." : "Return successful. Refund: " + refund);
-                        }
-                        case 5 -> {
-                            String ssn = validateNonEmpty("Customer SSN: ");
-                            LocalDate date = validateData("Purchase Date (dd-MM-yyyy): ");
-                            boolean paid = employee.applyPayment(ssn, date);
-                            System.out.println(paid ? "Payment applied." : "Payment failed.");
-                        }
-                        case 6 -> {
-                            CustomerProduct[] purchases = employee.getListOfPurchasingOperations();
-                            for (CustomerProduct cp : purchases) {
-                                System.out.println(cp.lineRepresentation());}}
-                        case 0 -> {
-                            System.out.println("Returning to main menu...");
-                            return;
-                        }
-                        default -> System.out.println("Invalid option.");
+
+                        if (choice2 == 7) break;
                     }
                 }
 
+                case 0 -> {
+                    System.out.println("System exited. Data saved.");
+                    return;
+                }
+
+                default -> System.out.println("Invalid option. Try again.");
             }
         }
-
-
     }
+
+
 }

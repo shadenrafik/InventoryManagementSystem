0b91bac22be2f0188ebee1c4da97e2915bd82093
Mohamed Bahig <mohamedbahigsaid@gmail.com>
2025-10-19 21:39:21 +0300

    EmployeeRole class still need to add 3 methods

---
+++ lab4/src/EmployeeRole.java
@@ -0,0 +1,62 @@
+package lab4;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class EmployeeRole {
+    private ProductDatabase productsDatabase;
+    private CustomerProductDatabase customerProductDatabase;
+
+    public EmployeeRole(){
+
+    }
+
+    public void addProduct(String productID, String productName, String manufacturerName, String supplierName, int quantity){
+        Product newProduct = new Product(productID,productName,manufacturerName,supplierName,quantity);
+        if(productDatabase.contains(productID)){
+            System.out.println("product ID already exixt");
+        }
+        else{
+            productsDatabase.insertRecord(newProduct);
+            productsDatabase.saveToFile();
+            System.out.println("Product added successfully");
+        }
+
+    }
+    
+    public Product[] getListOfProducts(){
+        ArrayList <product> records = productsDatabase.returnAllRecords();
+        Products[] productsArray = new product[records.size()];
+        for(int i =0 ; i<records.size();i++){
+            productsArray[i]= records.get(i);
+        }
+        return productsArray;
+
+    }
+
+    public CustomerProduct[] getListOfPurchasingOperations(){
+
+
+    }
+
+    public boolean purchaseProduct(String customerSSN, String productID, LocalDate purchaseDate){
+
+        
+    }
+
+    public double returnProduct(String customerSSN, String productID,LocalDate purchaseDate ,LocalDate returnDate){
+
+
+    }
+
+    public boolean applyPayment(String customerSSN, LocalDate purchaseDate){
+
+
+    }
+
+    public void logout(){
+        productsDatabase.saveToFile();
+        customerProductDatabase.saveToFile();
+        System.out.println("All Data was saved in productsDatabase and customerProductDatabase");
+    }
+}

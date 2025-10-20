68c5e9d1fb016da69070e62dd350bfe64759df0f
Mohamed Bahig <mohamedbahigsaid@gmail.com>
2025-10-20 08:49:16 +0300

    thx god done and tested

--- lab4/src/EmployeeRole.java
+++ lab4/src/EmployeeRole.java
@@ -15,12 +15,12 @@ public class EmployeeRole {
     public void addProduct(String productID, String productName, String manufacturerName, String supplierName, int quantity, float price){
         Product newProduct = new Product (productID,productName,manufacturerName,supplierName,quantity,price);
         if(productsDatabase.contains(productID)){
-            System.out.println("product ID already exixt");
+            System.out.println("product ID already exixt.");
         }
         else{
             productsDatabase.insertRecord(newProduct);
             productsDatabase.saveToFile();
-            System.out.println("Product added successfully");
+            System.out.println("Product added successfully.");
         }
 
     }
@@ -49,7 +49,7 @@ public class EmployeeRole {
     public boolean purchaseProduct(String customerSSN, String productID, LocalDate purchaseDate){
         Product product = productsDatabase.getRecord(productID);
         if(product==null){
-            System.out.println("Product not found");
+            System.out.println("Product not found.");
             return false;
         }
         if(product.getQuantity()== 0){
@@ -62,7 +62,7 @@ public class EmployeeRole {
         customerProductDatabase.insertRecord(newPurchase);
         productsDatabase.saveToFile();
         customerProductDatabase.saveToFile();
-        System.out.println("Purchase successful");
+        System.out.println("Purchase done.");
         return true;
 
 
@@ -70,11 +70,11 @@ public class EmployeeRole {
 
     public double returnProduct(String customerSSN, String productID,LocalDate purchaseDate ,LocalDate returnDate){
         if(returnDate.isBefore(purchaseDate)){
-            System.out.println("Return date cannot be before purchase date");
+            System.out.println("Return date cannot be before purchase date.");
             return -1;
         }
         if(!productsDatabase.contains(productID)){
-            System.out.println("Product not found");
+            System.out.println("Product not found.");
             return -1;
         }
 
@@ -83,14 +83,14 @@ public class EmployeeRole {
 
 
         if(!customerProductDatabase.contains(purchaseKey)){
-            System.out.println("Purchase record not found");
+            System.out.println("Purchase record not found.");
             return -1;
         }
 
         int daysBetween = (int) java.time.temporal.ChronoUnit.DAYS.between(purchaseDate, returnDate);   //mohim
 
         if(daysBetween >14){
-            System.out.println("more than 14 days have passed");
+            System.out.println("more than 14 days have passed.");
             return -1;
         }
 
@@ -98,7 +98,7 @@ public class EmployeeRole {
         product.setQuantity(product.getQuantity()+1);
         productsDatabase.saveToFile();
         customerProductDatabase.saveToFile();
-        System.out.println("Product returned successfully");
+        System.out.println("Product returned successfully.");
         return product.getPrice() ;
     }
 
@@ -109,16 +109,16 @@ public class EmployeeRole {
         for(int i =0 ; i<records.size();i++){
             if(records.get(i).getCustomerSSN().equals(customerSSN) && records.get(i).getPurchaseDate().equals(purchaseDate)){
                 if(records.get(i).isPaid()){
-                    System.out.println("already paid");
+                    System.out.println("already paid.");
                     return false;
                 }
                 records.get(i).setPaid(true);
                 customerProductDatabase.saveToFile();
-                System.out.println("paid successfully");
+                System.out.println("paid successfully.");
                 return true;
             }
         }
-        System.out.println("purchase record not found");
+        System.out.println("purchase record not found.");
         return false;
 
 
@@ -127,6 +127,6 @@ public class EmployeeRole {
     public void logout(){
         productsDatabase.saveToFile();
         customerProductDatabase.saveToFile();
-        System.out.println("All Data was saved in productsDatabase and customerProductDatabase");
+        System.out.println("All Data was saved in productsDatabase and customerProductDatabase. logging out.");
     }
 }
